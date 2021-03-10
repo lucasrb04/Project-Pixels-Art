90e6bd21e75d3e3b03e36f16ce1a1e98c9c66a9e
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-10 12:48:47 -0300

    Realizando requisito 6

---
+++ script.js
@@ -0,0 +1,7 @@
+function inciandoPixelArt() {
+  const element = document.querySelector('.color');
+  element.className += ' selected';
+}
+window.onload = inciandoPixelArt();
+function selected() {
+}
