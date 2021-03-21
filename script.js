c25f6d5448896b904d1f69d2dd247d791557455b
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-21 18:18:54 -0300

    iniciando funçao de aumentar o quadro

--- script.js
+++ script.js
@@ -32,4 +32,59 @@ function clear() {
     pixel[i].style.backgroundColor = 'white';
   }
 }
-document.querySelector('#clear-board').addEventListener('click', clear);
+
+function createTr(boardSize) {
+  let counte = 0;
+  const pixelBoard = document.querySelector('#pixel-board');
+  while (counte < boardSize) {
+    const createTrItem = document.createElement('tr');
+    pixelBoard.appendChild(createTrItem);
+    counte += 1;
+  }
+}
+function createTd() {
+  const trItem = document.querySelectorAll('tr');
+  for (let i = 0; i < trItem.length; i += 1) {
+    let count = 0;
+    while (count < trItem.length) {
+      const createTdItem = document.createElement('td');
+      createTdItem.className = 'pixel';
+      trItem[i].appendChild(createTdItem);
+      count += 1;
+    }
+  }
+}
+function clearTr() {
+  const pixelBoard = document.querySelectorAll('tr');
+  for (let index = 0; index < pixelBoard.length; index += 1) {
+    pixelBoard[index].remove();
+  }
+}
+const tableBtn = document.querySelector('#generate-board');
+
+tableBtn.addEventListener('click', function() {
+  clearTr();
+});
+
+tableBtn.addEventListener('click', function() {
+  createTr(document.querySelector('#board-size').value);
+});
+
+tableBtn.addEventListener('click', function() {
+  createTd();
+});
+
+createTr(5);
+createTd();
+
+const clearBtn = document.querySelector('#clear-board');
+clearBtn.addEventListener('click', clear);
+
+// const inputFf = document.querySelector('#font-family');
+// function changeFf(fontFamily) {
+//   document.querySelector('p').style.fontFamily = (inputFf.value);
+//   localStorage.setItem('font-family', fontFamily);
+// }
+
+// inputFf.addEventListener('change',function() {
+// changeFf(inputFf.value)});
